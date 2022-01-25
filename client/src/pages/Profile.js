d64ac5e63b83467da926b41c07e74fac96f96e19
AndiS90 <87630680+AndiS90@users.noreply.github.com>
2022-01-24 19:00:13 -0600

    working on api integration and font integration along with upgrading to bcryptjs

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+
 import { Redirect, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
+import { getNamesPlusNullArray } from '../utils/API';
+import { Autocomplete } from 'react-materialize';
+import 'materialize-css';
 
 import SkillsList from '../components/SkillsList';
 import SkillForm from '../components/SkillForm';
@@ -10,9 +13,18 @@ import SkillForm from '../components/SkillForm';
 import { QUERY_SINGLE_PROFILE, QUERY_ME } from '../utils/queries';
 
 import Auth from '../utils/auth';
-
 const Profile = () => {
   const { profileId } = useParams();
+
+
+const optionsObj = getNamesPlusNullArray();
+
+console.log( optionsObj );
+
+
+
+
+
 
   // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
   const { loading, data } = useQuery(
@@ -49,6 +61,18 @@ const Profile = () => {
       <h2 className="card-header">
         This is your Dashboard! You can do whatever you want with it!
       </h2>
+
+      <Autocomplete
+  id="Autocomplete-41"
+  options={{
+    data: 
+    optionsObj
+  }}
+  placeholder="Insert here"
+  title="Input Label"
+/>
+
+
 
       {/* {profile.skills?.length > 0 && (
         <SkillsList
